de86d42259cd9717230283f5e9250b62c8bb9f72
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-08-10 11:51:47 +0530

    create db and db backup

---
+++ 1.Create & Database Backup.sql
@@ -0,0 +1,28 @@
+
+create database Ajay2024
+go
+
+use Ajay2024
+execute sp_renamedb Ajay2024, Ajaynew2024;
+
+--one machiene to another machiene migrate database
+
+--Full backup
+backup database Ajay2024 to disk = 'D:\Coding2\Ajay V#\SQL.bak'
+
+--differential backup   (repeat user)
+
+backup database Ajay2024 to disk = 'D:\Coding2\Ajay V#\SQL.bak'
+
+--restore
+
+use master
+
+drop database Ajay2024
+
+restore database Ajay2024 from disk = 'D:\Coding2\Ajay V#\SQL.bak'
+
+use Ajay2024
+go
+
+
