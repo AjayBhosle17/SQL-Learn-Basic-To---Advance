02a088aacfdbde6c508a7706d2a455327c50b05c
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2025-03-23 07:12:29 +0530

    Group By ANd Having Clause

--- Revision/06_Group_By.sql
+++ Revision/06_Group_By.sql
@@ -1,4 +1,4 @@
-use abdtimepass
+﻿use abdtimepass
 
 drop table student
 
@@ -48,6 +48,7 @@ select city , gender , count(rollno)as total
 from student
 group by city , gender
 
+select * from student
 -- city wise minumum age
 
 select gender , min(age) 
@@ -58,3 +59,137 @@ group by gender
 select city , min(age) 
 from student
 group by  city
+
+
+-- city wise no of student return only that city which has equal  or more than 5 student
+
+select city, count(city) as cnt 
+from student
+group by city 
+
+select city, count(city) as cnt 
+from student
+group by city 
+having count(city)>=4
+
+/*
+1. Count Students in Each Course
+You have a students table with the following columns:
+
+id (int)
+
+name (varchar)
+
+course (varchar)
+
+👉 Write a query to count how many students are enrolled in each course.*/
+
+SELECT course, COUNT(*) AS student_count  
+FROM students  
+GROUP BY course;
+
+/*
+Output:=> 
+📌 Example sales Table
+id	product	quantity_sold
+1	Laptop	30
+2	Phone	50
+3	Laptop	40
+4	Tablet	20
+5	Phone	60
+6	Tablet	15
+✅ Query Output:
+product	total_sold
+Laptop	70
+Phone	110
+Tablet	35
+So, only "Phone" will be included in the result because its total quantity sold is 110 (which is ≥ 100).
+*/
+
+/*
+2. Find Products with High Sales
+You have a sales table:
+
+id (int)
+
+product_name (varchar)
+
+quantity_sold (int)
+
+👉 Write a query to find products that have sold more than 100 units.
+*/
+
+select product, sum(quantity_sold) from sales
+group by product
+having sum(quantity_sold)>=100
+
+/* Output=> 
+📌 Example sales Table
+id	product	quantity_sold
+1	Laptop	30
+2	Phone	50
+3	Laptop	40
+4	Tablet	20
+5	Phone	60
+6	Tablet	15
+✅ Expected Output
+product	total_sold
+Phone	110*/
+
+/*
+3. Identify High-Spending Customers
+You have a orders table:
+
+order_id (int)
+
+customer_name (varchar)
+
+total_amount (decimal)
+
+👉 Write a query to find customers who have placed at least 5 orders.*/
+
+
+select customer_name , count(*)
+from Orders
+group by customer_name
+having count(*)>=5
+
+/*
+4. Average Salary in Each Department
+You have an employees table:
+
+id (int)
+
+name (varchar)
+
+department (varchar)
+
+salary (decimal)
+
+👉 Write a query to calculate the average salary per department, and filter only those departments where the average salary is above 50,000.
+
+*/
+
+SELECT department, AVG(salary) AS avg_salary  
+FROM employees  
+GROUP BY department  
+HAVING AVG(salary) > 50000;
+
+
+
+/*5. Most Popular Cities for Customers
+You have a customers table:
+
+id (int)
+
+name (varchar)
+
+city (varchar)
+
+👉 Write a query to list only those cities that have at least 10 customers.*/
+
+
+select city, count(*)
+from customers
+group by city
+having count(*)>=10
